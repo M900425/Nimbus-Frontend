e52280b8309781dd2f71122c878bd8a1297a57ce
M900425 <ifsubdtj@gmail.com>
2026-07-23 17:06:19 +0300

    Geocoding fixes

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { weatherApi } from "./api/weatherApi";
+import { geocodingApi } from "./api/geocodingApi";
 import uiReducer from "./slices/uiSlice";
 
 export const store = configureStore({
   reducer: {
     ui: uiReducer,
     [weatherApi.reducerPath]: weatherApi.reducer,
+    [geocodingApi.reducerPath]: geocodingApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(weatherApi.middleware),
+    getDefaultMiddleware()
+      .concat(weatherApi.middleware)
+      .concat(geocodingApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
